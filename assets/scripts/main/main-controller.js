97621bfa62fa40546e3dd725f9634016aade119e
Shist <shist.pupust@mail.ru>
2023-07-29 21:04:00 +0300

    Added modal windows in cases of guest mode and cancellation of typing

--- assets/scripts/main/main-controller.js
+++ assets/scripts/main/main-controller.js
@@ -97,35 +97,115 @@ function initMain() {
     btnStartTyping.addEventListener("click", () => {
       typingInput.value = "";
       if (btnStartTyping.textContent === "Start typing") {
-        btnStartTyping.textContent = "Cancel typing";
-        getTextForTyping(sentencesAmountSlider.value).then((wordsArr) => {
-          typingTextWrapper.innerHTML = "";
-          currWordsArr = wordsArr;
-          startTime = new Date().getTime();
-          intervalId = setInterval(updateTimerAndSpeed, 10);
-          currWholeCharsCount = getWholeCharsAmount(wordsArr);
-          updateSymbolsAmount(0);
-          currWordsArr.forEach((word, index) => {
-            const nextWord = document.createElement("span");
-            nextWord.classList.add("typing-word");
-            nextWord.id = `word-${index}`;
-            nextWord.textContent = word;
-            typingTextWrapper.append(nextWord);
+        const modalWindowWrapper = document.createElement("div");
+        modalWindowWrapper.classList.add("modal-window-wrapper");
+        modalWindowWrapper.innerHTML = `
+        <div class="modal-window-wrapper__window">
+            <button class="modal-window-wrapper__close-btn"></button>
+            <h3 class="modal-window-wrapper__headline">Attention</h3>
+            <p class="modal-window-wrapper__text-info">
+                You are in the guest mode at the moment. In this mode you cannot save the statistics of your workouts 
+                in the personal account. Would you like to sign in or start typing anyway?
+            </p>
+            <div class="modal-window-wrapper__btns-wrapper">
+                <a href="./personal-area.html" class="modal-window-wrapper__btn-sign-in">Sign in</a>
+                <button class="modal-window-wrapper__btn-start-anyway">Start anyway</button>
+            </div>
+        </div>
+        `;
+        document.body.append(modalWindowWrapper);
+        const modalWindowCloseBtn = document.querySelector(
+          ".modal-window-wrapper__close-btn"
+        );
+        modalWindowCloseBtn.addEventListener("click", () => {
+          modalWindowWrapper.remove();
+        });
+        modalWindowWrapper.addEventListener("click", (event) => {
+          if (event.target === modalWindowWrapper) {
+            modalWindowWrapper.remove();
+          }
+        });
+        const modalWindowSignInBtn = document.querySelector(
+          ".modal-window-wrapper__btn-sign-in"
+        );
+        modalWindowSignInBtn.addEventListener("click", () => {
+          document.href = "./personal-area.html";
+        });
+        const modalWindowStartAnywayBtn = document.querySelector(
+          ".modal-window-wrapper__btn-start-anyway"
+        );
+        modalWindowStartAnywayBtn.addEventListener("click", () => {
+          modalWindowWrapper.remove();
+          btnStartTyping.textContent = "Cancel typing";
+          getTextForTyping(sentencesAmountSlider.value).then((wordsArr) => {
+            typingTextWrapper.innerHTML = "";
+            currWordsArr = wordsArr;
+            startTime = new Date().getTime();
+            intervalId = setInterval(updateTimerAndSpeed, 10);
+            currWholeCharsCount = getWholeCharsAmount(wordsArr);
+            updateSymbolsAmount(0);
+            currWordsArr.forEach((word, index) => {
+              const nextWord = document.createElement("span");
+              nextWord.classList.add("typing-word");
+              nextWord.id = `word-${index}`;
+              nextWord.textContent = word;
+              typingTextWrapper.append(nextWord);
+            });
+            document.querySelector("#word-0").classList.add("active-word");
+            sentencesAmountSlider.disabled = true;
+            typingInput.disabled = false;
+            typingInput.focus();
           });
-          document.querySelector("#word-0").classList.add("active-word");
-          sentencesAmountSlider.disabled = true;
-          typingInput.disabled = false;
-          typingInput.focus();
         });
       } else {
-        btnStartTyping.textContent = "Start typing";
-        typingInput.disabled = true;
-        sentencesAmountSlider.disabled = false;
-        typingInput.classList.remove("input-with-error");
-        typingTextWrapper.innerHTML = `
-              <span class="main__typing-text-start-msg">Here will be the text . . .</span>
-          `;
-        setAllStatsToDefault(intervalId, typingTextWrapper);
+        const modalWindowWrapper = document.createElement("div");
+        modalWindowWrapper.classList.add("modal-window-wrapper");
+        modalWindowWrapper.innerHTML = `
+        <div class="modal-window-wrapper__window">
+            <button class="modal-window-wrapper__close-btn"></button>
+            <h3 class="modal-window-wrapper__headline">Attention</h3>
+            <p class="modal-window-wrapper__text-info">
+                You have not completed your workout yet. If you cancel it you will lose your progress 
+                and the text will have to be typed again. Continue cancellation?
+            </p>
+            <div class="modal-window-wrapper__btns-wrapper">
+                <button class="modal-window-wrapper__btn-resume-typing">Resume typing</button>
+                <button class="modal-window-wrapper__btn-confirm-cancellation">Confirm cancellation</button>
+            </div>
+        </div>
+        `;
+        document.body.append(modalWindowWrapper);
+        const modalWindowCloseBtn = document.querySelector(
+          ".modal-window-wrapper__close-btn"
+        );
+        modalWindowCloseBtn.addEventListener("click", () => {
+          modalWindowWrapper.remove();
+        });
+        modalWindowWrapper.addEventListener("click", (event) => {
+          if (event.target === modalWindowWrapper) {
+            modalWindowWrapper.remove();
+          }
+        });
+        const modalWindowResumeTypingBtn = document.querySelector(
+          ".modal-window-wrapper__btn-resume-typing"
+        );
+        modalWindowResumeTypingBtn.addEventListener("click", () => {
+          modalWindowWrapper.remove();
+        });
+        const modalWindowConfirmCancellationBtn = document.querySelector(
+          ".modal-window-wrapper__btn-confirm-cancellation"
+        );
+        modalWindowConfirmCancellationBtn.addEventListener("click", () => {
+          modalWindowWrapper.remove();
+          btnStartTyping.textContent = "Start typing";
+          typingInput.disabled = true;
+          sentencesAmountSlider.disabled = false;
+          typingInput.classList.remove("input-with-error");
+          typingTextWrapper.innerHTML = `
+                  <span class="main__typing-text-start-msg">Here will be the text . . .</span>
+              `;
+          setAllStatsToDefault(intervalId, typingTextWrapper);
+        });
       }
     });
 
